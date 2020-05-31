cd85a53053499e616025f70a53f4ff81f4aaf636
Pedro The Dev <47012647+LuisTheDeveloper@users.noreply.github.com>
2020-05-31 14:22:47 +0100

    Array destructuring

---
+++ index-1-1e-destructuring.js
@@ -0,0 +1,8 @@
+//Array destructuring is an important concept in React
+// We manage state in React by using destructuring.
+
+const [, , fruit, , book] = ["cow", "glass", "strawberry", "dumbbell", "my book"];
+
+console.log(fruit)
+
+console.log(book)
